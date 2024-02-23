8344f2a6e3c963cf44148e0e8f74af4ca1fc738e
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2024-02-23 11:20:51 -0800

    intial react app creation and app route addition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/register" element={<Register />}/>
+      <AuthorizedRoute>
+        {/*Reserved for protected route for edit post*/}
+      </AuthorizedRoute>
+      <Route path="/editpost/:id" element={
+        <AuthorizedRoute>
+        {/*Reserved for protected <AddEditPost/>*/}
+      </AuthorizedRoute>
+      }
+      />
+     </Routes>
     </div>
   );
 }
 
 export default App;
+576
